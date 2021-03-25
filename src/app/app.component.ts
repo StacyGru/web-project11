5903003fa6c90a238453f9f4be67296aa9ee7ddc
StacyGru <stacy.gru@gmail.com>
2021-03-25 10:45:01 +0300

    Лекция

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'project11';
+  myNumber = 10;
+  flag = true;
+  buttonEnabled = false;
+ 
+  blockX = 0;
+  blockY = 0;
+
+  inputValue = 'text';
+
+  @ViewChild('someInput') someInputName: ElementRef | undefined;
+
+  someArray: string[] = ['первый', 'второй', 'третий'];
+
+  fontSize = 15;
+
+  constructor()
+  {
+    setTimeout(() =>
+    {
+      this.buttonEnabled = true;
+      this.fontSize = 20;
+    }, 1500);
+  }
+
+  ngOnInit()
+  {
+    console.log(this.someInputName);
+  }
+
+  ngAfterViewInit()
+  {
+    console.log(this.someInputName);
+  }
+
+  hello()
+  {
+    return 'Hello world';
+  }
+
+  getCoords(e: MouseEvent)
+  {
+    this.blockX = e.offsetX;
+    this.blockY = e.offsetY;
+  }
+
+  getData(elm: HTMLInputElement)
+  {
+    // console.log(elm);
+    console.log(this.someInputName?.nativeElement.value);
+  }
 }
